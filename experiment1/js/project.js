6e341892fedc4ddff9fbf8672702b3b5c7e1cde3
aidansterling6 <aidansterling6@gmail.com>
2024-04-07 01:40:46 -0700

    update

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,194 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+const fillers = {
+  createCanvas: [
+    "createCanvas(400, 400);",
+    "createCanvas(500, 500);",
+    "createCanvas(600, 600);",
+    "createCanvas(800, 800);",
+    "createCanvas(random(300,500), random(300,500));",
+  ],
+  sx: [
+    "width/2;",
+    "random(0,width);",
+  ],
+  sy: [
+    "height/2;",
+    "random(0,height);",
+  ],
+  svx: [
+    "20;",
+    "random(-5,5);",
+    "random(-10,10);"
+  ],
+  svy: [
+    "20;",
+    "random(-5,5);",
+    "random(-10,10);"
+  ],
+  startgrav: [
+    `
+    ax = random(-1,1);
+    ay = random(-1,1);
+    `,
+    `
+    ax = 0;
+    ay = -0.4;
+    `,
+    `
+    ax = 0;
+    ay = 0.4;
+    `,
+    `
+    ax = -0.4;
+    ay = 0;
+    `,
+    `
+    ax = 0.4;
+    ay = 0;
+    `,
+  ],
+  color: [
+    "255",
+    "random(50,200)",
+    "255, 0, 0",
+    "255, 0, 255",
+    "30, 30, 30, 100",
+    "255, 255, 255, 200",
+    "255, 255, 255, 100",
+    "random(50,200), random(50,200), random(50,200), 100",
+    "random(50,200), random(50,200), random(50,200), 255",
+    "random(50,200), random(50,200), random(50,200), 20",
+    "random(50,200), random(50,200), random(50,200), 75"
+  ],
+  shape: [
+    "ellipse",
+    "rect"
+  ],
+  effect: [
+    "ax += random(-0.1,0.1);",
+    "ay += random(-0.1,0.1);",
+    "vx += random(-0.1,0.1);",
+    "vy += random(-0.1,0.1);",
+    "X += 5",
+    "X -= 5",
+    "Y += 5",
+    "Y -= 5",
+    `
+    translate(width/2, height/2);
+    rotate($c);
+    translate(-width/2, -height/2);
+    `,
+    `
+    line(0,0,X,Y);
+    line(width,0,X,Y);
+    line(0,height,X,Y);
+    line(width,height,X,Y);
+    `,
+    `
+    $shape(width - X, height - Y, Size, Size);
+    `,
+    `
+    $shape(width/2 + vx, height/2 + vy, Size, Size);
+    `
+  ],
+  c: [
+    "X",
+    "Y",
+    "vx",
+    "vy"
+  ]
+};
+const template = 
+`
+var X;
+var Y;
+var Size;
+var vx;
+var vy;
+var ax;
+var ay;
+var c1;
+var c2;
+var c3;
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+function setup() {
+  $createCanvas
+  
+  c1 = $color;
+  c2 = $color;
+  c3 = $color;
+  
+  Size = random(20,50);
+  
+  X = $sx
+  Y = $sy
+  
+  vx = $svx
+  vy = $svy
+  
+  $startgrav
+  
+}
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
+function draw() {
+  background(c1);
+  vx += ax;
+  vy += ay;
+  
+  X += vx;
+  Y += vy;
+  
+  if(X > width - Size/2){
+    X = width - Size/2;
+    vx = -abs(vx);
+  }
+  if(X < Size/2){
+    X = Size/2;
+    vx = abs(vx);
+  }
+  if(Y > height - Size/2){
+    Y = height - Size/2;
+    vy = -abs(vy);
+  }
+  if(Y < Size/2){
+    Y = Size/2;
+    vy = abs(vy);
   }
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+  $effect
+  $effect
+  $effect
+  
+  rectMode(CENTER);
+  fill(c2);
+  stroke(c3);
+  $shape(X, Y, Size, Size);
+}
+`;
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
